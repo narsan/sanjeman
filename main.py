f0322b1bae70ed2a26c9e5bf2e767f690e5cf887
narges <nsodeifi78@gmail.com>
2021-08-22 21:15:20 +0430

    main.py updated

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
-# coding=utf-8
-# This is a sample Python script.
+import mysql.connector
+import pandas
+from sklearn import tree
+import pydotplus
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from mysql.connector import connect, Error
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1378@1378",
+    database="sanjeman",
+    use_pure=True
+)
+
+my_cursor = mydb.cursor()
+steps_title_list = []
+d_steps_title = {}
+university_name_list = []
+d_uni_name = {}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+my_cursor.execute('select distinct steps_title from useful_data')
+for item in my_cursor:
+    steps_title_list.append(item)
+
+my_cursor.execute('select distinct university from useful_data')
+for item in my_cursor:
+    university_name_list.append(item)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+df = pandas.read_csv("Result_41.csv")
+for title in steps_title_list:
+    if title[0] == 'نیازمند تعیین وضعیت':
+        d_steps_title[title[0]] = 0
+    if title[0] == 'تایید برای مصاحبه':
+        d_steps_title[title[0]] = 1
+    if title[0] == 'استخدام شده':
+        d_steps_title[title[0]] = 2
+    if title[0] == 'رد شده':
+        d_steps_title[title[0]] = 3
+    if title[0] == 'انصراف از مصاحبه':
+        d_steps_title[title[0]] = 4
+    else:
+        d_steps_title[title[0]] = 6
+i = 0
+for title in university_name_list:
+    d_uni_name[title] = i
+    i += 1
+print(d_steps_title)
+df['steps_title'] = df['steps_title'].map(d_steps_title)
+df['university'] = df['university'].map(d_uni_name)
+print(df['university'])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
