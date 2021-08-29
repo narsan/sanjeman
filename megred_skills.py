b1f4cb4cfbef0c82a03c06c6b14f654bc8bc66c1
Narges Sodeifi <46598182+narsan@users.noreply.github.com>
2021-08-29 12:06:41 +0430

    merged skills updated

--- megred_skills.py
+++ megred_skills.py
@@ -15,16 +15,20 @@ mydb = mysql.connector.connect(
     use_pure=True
 )
 
-merged_skill_dict = {}
 my_cursor = mydb.cursor()
 df = pandas.read_csv("Result_32.csv")
 
 
-my_cursor.execute("SELECT job_applicant_id, GROUP_CONCAT(distinct skills_title SEPARATOR ', ') FROM useful_data GROUP BY job_applicant_id;")
-for item in my_cursor:
-    merged_skill_dict[item[0]] = item[1]
+def merge_skills():
+    merged_skill_dict = {}
+    my_cursor.execute("SELECT job_applicant_id, GROUP_CONCAT(distinct skills_title SEPARATOR ', ') FROM useful_data GROUP BY job_applicant_id;")
+    for item in my_cursor:
+        merged_skill_dict[item[0]] = item[1]
+    l_df = pandas.DataFrame.from_dict(merged_skill_dict, orient='index')
+    l_df.columns = ['merged_skills']
+    return l_df
 
-l_df = pandas.DataFrame.from_dict(merged_skill_dict, orient='index')
-l_df.columns = ['merged_skills']
-merged = df.merge(l_df, left_on='job_applicant_id', right_index=True, how='left')
-merged.to_csv('mrg_skill.csv', encoding='utf-8')
+
+left_df = merge_skills()
+merged = df.merge(left_df, left_on='job_applicant_id', right_index=True, how='left')
+merged.to_csv('merge.csv', encoding='utf-8')
