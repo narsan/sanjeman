fd89d0a10c0892e7968148a7002ad9007c8b0f04
mahshid rahmani <mahshidrahmani2001@gmail.com>
2021-08-30 10:29:11 +0430

    marriage status added to final data

--- megred_skills.py
+++ megred_skills.py
@@ -7,7 +7,7 @@ from datetime import date
 
 skills = dict()
 education = dict()
-header = ['job_applicant_id', 'gender', 'age', 'education', 'skill', 'steps_title']
+header = ['job_applicant_id', 'gender', 'age', 'education', 'skill', 'marriage_status', 'steps_title']
 
 
 with open('pss.txt') as file:
@@ -51,6 +51,16 @@ def merge_education():
             education[item[0]] = item[1]
 
 
+def get_marriage_status(marriage):
+    if not marriage:
+        return -1
+    marriage_status = int.from_bytes(marriage, "big") - ord('0')
+    if marriage_status != 0 and marriage != 1:
+        return -1
+
+    return marriage
+
+
 def calculate_age(born):
     if not born:
         return None
@@ -62,21 +72,24 @@ def calculate_age(born):
 def get_applicant_info(job_applicant_id):
     score_skills = skills[job_applicant_id]
     score_education = education[job_applicant_id]
-    my_cursor.execute("SELECT DISTINCT gender, birthday, steps_title FROM useful_data WHERE " + str(job_applicant_id) + " = job_applicant_id;")
+    query = "SELECT DISTINCT gender, birthday, JSON_UNQUOTE(json_extract(marriage,'$.status')), steps_title " + \
+            "FROM useful_data WHERE " \
+            + str(job_applicant_id) + " = job_applicant_id;"
+
+    my_cursor.execute(query)
     for item in my_cursor:
-        return item[0], calculate_age(item[1]), item[2]
+        return item[0], calculate_age(item[1]), get_marriage_status(item[2]), item[3]
 
 
 def write_file():
-
     with open('final_data.csv', 'w', encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(header)
         for job_applicant_id in skills.keys():
             score_skills = skills[job_applicant_id]
             score_education = education[job_applicant_id]
-            gender, age, steps_title = get_applicant_info(job_applicant_id)
-            writer.writerow([job_applicant_id, gender, age, score_education, score_skills, steps_title])
+            gender, age, marriage, steps_title = get_applicant_info(job_applicant_id)
+            writer.writerow([job_applicant_id, gender, age, score_education, score_skills, marriage, steps_title])
 
 
 merge_skills()
